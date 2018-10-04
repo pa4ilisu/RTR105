cce52d7190fc40ce2a3ff088e09c0a21352257ed
user <jazixaa@inbox.lv>
2018-10-04 11:51:56 +0300

    20181004_11_51

---
+++ dialogs.py
@@ -0,0 +1,7 @@
+#!/usr/bim/python
+
+a = input("Cienijamais lietotajs ludzu ievadi skaitli: ")
+a = int(a)
+print("Liettotajs tu esi ivadijs skaitlis: %d"%(a))
+aa = a * a
+print("tavs skaitlis kvadrata ir: %d "%(aa))
