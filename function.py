41d5ef74b72ee232b090a68b5dbe1e5b2bf079be
user <jazixaa@inbox.lv>
2018-10-18 11:35:57 +0300

    20181018_11_35

---
+++ function.py
@@ -0,0 +1,26 @@
+print(float(99)/100)
+i=42
+type(i)
+f=float (i)
+print(f)
+type(f)
+print(1+
+    2*float(3)/4-5)
+
+#otrais testejums
+'''
+big = max("Hello Viktorija111")
+print(big)
+tiny = min("Hello Viktorija111")
+print(tiny)
+'''
+
+#pirmais testejums
+'''
+def thing():
+    print("Hello")
+    print("Fun")
+thing()
+print("zip")
+thing()
+'''
