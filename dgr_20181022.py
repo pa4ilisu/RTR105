fc56fb20f5a5db65e58e0187877eaf601a5124d1
user <jazixaa@inbox.lv>
2018-10-22 15:11:28 +0300

    20181022_15_11

---
+++ dgr_20181022.py
@@ -0,0 +1,54 @@
+
+# 5 meiginajums
+'''
+zork = 0
+print('Before',zork)
+for thing in [9,41,12,3,74,15]:
+    zork = zork + 1
+    print(zork,thing)
+print('After',zork)
+'''
+#4 meiginajums
+'''
+largest_so_far = -1
+print('Before',largest_so_far)
+for the_num in [9,41,12,3,74,15]:
+    if the_num > largest_so_far:
+        largest_so_far = the_num
+    print(largest_so_far,the_num)
+
+print('After',largest_so_far)
+'''
+# 3 meiginajums 
+'''
+friends = ['Laaaaacitis','Viktorija111','Daniels']
+for friend in friends :
+    print('Happy new year:',friend)
+print('Done!')
+'''
+# 2 meiginajums
+'''
+x = 12
+if x ==5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or Equals 5')
+if x < 6 : print ('Less than 6')
+if x <= 5:
+    print('Less than equals 5')
+if x != 6:
+    print('Not equal 6')
+'''
+
+#1 meiginajums
+'''
+n = -1
+while n > 0:
+    print(n)
+    n = n -1
+print('Blastoff!')
+print(n)
+'''
+    
